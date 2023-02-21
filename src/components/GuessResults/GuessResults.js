ca42f37da7e375f07586a658c59ca0f36dcc5cd8
Abhay <abhaykv04@gmail.com>
2023-02-22 01:22:55 +0530

    Use num of guesses constant

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import { range } from "../../utils";
 import Guess from "../Guess/Guess";
 
 function GuessResults({ guesses, answer }) {
   return (
     <div className="guess-results">
-      {range(0, 6).map((_, index) => (
+      {range(0, NUM_OF_GUESSES_ALLOWED).map((_, index) => (
         <Guess key={index} guess={guesses[index]?.label} answer={answer} />
       ))}
     </div>
